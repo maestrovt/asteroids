636c7f5d9b8f37595a8d9f9959203b3c69800000
maestrovt <maestrovt@gmail.com>
2024-09-12 05:43:30 -0400

    Fixed asteroid radius, added shot cooldown period

--- player.py
+++ player.py
@@ -8,6 +8,7 @@ class Player(CircleShape):
     def __init__(self, x, y):
         super().__init__(x, y, PLAYER_RADIUS)
         self.rotation = 0
+        self.shooting_timer = 0
 
     def draw(self, screen):
         pygame.draw.polygon(screen, "white",  self.triangle(), 2)
@@ -21,6 +22,7 @@ class Player(CircleShape):
         return [a, b, c]
 
     def update(self, dt):
+        self.shooting_timer -= dt
         keys = pygame.key.get_pressed()
         if keys[pygame.K_a]:
             self.rotate(-dt)
@@ -31,7 +33,9 @@ class Player(CircleShape):
         if keys[pygame.K_s]:
             self.move(-dt)
         if keys[pygame.K_SPACE]:
-            self.shoot()
+            if self.shooting_timer <= 0:
+                self.shoot()
+                self.shooting_timer = PLAYER_SHOOT_COOLDOWN
 
     def move(self, dt):
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
